f4209a9a18af7cccddc1557e92998a1d0f09753b
askiada <25521495+askiada@users.noreply.github.com>
2024-10-26 22:02:11 +0200

    skip sort

--- pkg/chunksorter/chunksorter.go
+++ pkg/chunksorter/chunksorter.go
@@ -64,81 +64,83 @@ func (c *ChunkSorter) validate() error {
 // Sort sorts the chunks.
 func (c *ChunkSorter) Sort(ctx context.Context, rdr model.Reader) (model.Reader, error) {
 
-	err := c.validate()
-	if err != nil {
-		return nil, fmt.Errorf("failed to validate ChunkSorter: %w", err)
-	}
-
-	c.trace("creating output buffer")
-
-	buffer := c.vectorFn(c.keyFn)
-	if buffer == nil {
-		return nil, ErrNilVector
-	}
-
-	c.trace("output buffer created")
-
-outer:
-	for {
-
-		select {
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		default:
-			foundNew := rdr.Next()
-			if !foundNew {
-				break outer
-			}
-
-			row, n, err := rdr.Read()
+	//TODO: skip sorting if chunk is already sorted
+	/*
+			err := c.validate()
 			if err != nil {
-				return nil, err
+				return nil, fmt.Errorf("failed to validate ChunkSorter: %w", err)
 			}
 
-			c.tracef("pushing row %v to buffer", row)
-			err = buffer.PushBack(row, n)
-			if err != nil {
-				c.logger.Errorf("failed to push row [%+v] to buffer: %w", row, err)
+			c.trace("creating output buffer")
 
-				continue outer
+			buffer := c.vectorFn(c.keyFn)
+			if buffer == nil {
+				return nil, ErrNilVector
 			}
-		}
-	}
 
-	if rdr.Err() != nil {
-		return nil, fmt.Errorf("failed to read row: %w", rdr.Err())
-	}
+			c.trace("output buffer created")
 
-	c.trace("sorting buffer")
-	buffer.Sort()
+		outer:
+			for {
 
+				select {
+				case <-ctx.Done():
+					return nil, ctx.Err()
+				default:
+					foundNew := rdr.Next()
+					if !foundNew {
+						break outer
+					}
+
+					row, n, err := rdr.Read()
+					if err != nil {
+						return nil, err
+					}
+
+					c.tracef("pushing row %v to buffer", row)
+					err = buffer.PushBack(row, n)
+					if err != nil {
+						c.logger.Errorf("failed to push row [%+v] to buffer: %w", row, err)
+
+						continue outer
+					}
+				}
+			}
+
+			if rdr.Err() != nil {
+				return nil, fmt.Errorf("failed to read row: %w", rdr.Err())
+			}
+
+			c.trace("sorting buffer")
+			buffer.Sort()
+	*/
 	c.trace("creating chunk writer")
 	chunkIdx, wr, err := c.chunkWriterFn()
 	if err != nil {
 		return nil, fmt.Errorf("failed to create chunk writer (previous chunkID %d): %w", chunkIdx, err)
 	}
+	/*
+		c.debugf("chunk writer created idx: %d", chunkIdx)
 
-	c.debugf("chunk writer created idx: %d", chunkIdx)
-
-	c.trace("writing rows")
-	for i := range buffer.Len() {
-		select {
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		default:
-			c.tracef("writing row %d", i)
-			err := wr.WriteRow(ctx, buffer.Get(i).Row)
-			if err != nil {
-				return nil, fmt.Errorf("failed to write row [%+v] in chunk %d: %w", buffer.Get(i).Row, chunkIdx, err)
+		c.trace("writing rows")
+		for i := range buffer.Len() {
+			select {
+			case <-ctx.Done():
+				return nil, ctx.Err()
+			default:
+				c.tracef("writing row %d", i)
+				err := wr.WriteRow(ctx, buffer.Get(i).Row)
+				if err != nil {
+					return nil, fmt.Errorf("failed to write row [%+v] in chunk %d: %w", buffer.Get(i).Row, chunkIdx, err)
+				}
 			}
 		}
-	}
 
-	c.trace("resetting output buffer")
-	buffer.Reset()
+		c.trace("resetting output buffer")
+		buffer.Reset()
 
-	buffer = nil
-
+		buffer = nil
+	*/
 	c.trace("closing chunk writer after sorting")
 	err = wr.Close()
 	if err != nil {
